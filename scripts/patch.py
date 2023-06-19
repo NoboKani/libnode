1a087c251f58c18e29e7e2ad028c2c668164bcae
NoboKani <nobokani@ya.ru>
2023-06-19 20:20:25 +0300

    test build

--- scripts/patch.py
+++ scripts/patch.py
@@ -9,4 +9,8 @@ print(os.getcwd())
 
 shutil.copyfile('../patch/node/src/node_embedding_api.cc', './src/node_embedding_api.cc')
 shutil.copyfile('../patch/node/src/node_embedding_api.h',  './src/node_embedding_api.h')
-subprocess.check_call(['patch', '-p1', '-i', '../patch/node.patch'])
+
+try:
+    subprocess.check_call(['patch', '-p1', '-i', '../patch/node.patch'])
+except subprocess.CalledProcessError:
+    print("error")
